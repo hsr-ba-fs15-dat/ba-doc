5ef016a655e7bc2b17653ddcca0888af20bc372f
Fabio Scala <fabio.scala@gmail.com>
2015-02-15 22:58:49 +0100

    full pdf & deploy artifact

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+(
+mkdir out
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "build@travis-ci.org"
+cp ../main.pdf ./
+git add .
+git commit -m "Deployed to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/hsr-ba-fs15-dat/ba-doc.git" master:gh-pages > /dev/null 2>&1
+)
