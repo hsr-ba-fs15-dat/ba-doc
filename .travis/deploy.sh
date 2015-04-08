4e129aae20d20a004eefd8f3f7c4b32c22a279a1
Christoph Hüsler <christoph.huesler@burnstone.ch>
2015-04-08 12:30:26 +0200

    Deploy interim presentation as well

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,6 +7,8 @@ git config user.email "build@travis-ci.org"
 cp ../main.pdf ./
 mkdir sitzungsprotokolle
 cp ../content/projektmanagement/sitzungsprotokolle/protokoll-*.pdf ./sitzungsprotokolle
+mkdir praesentationen
+cp -r ../presentation/ praesentationen
 git add .
 git commit -m "Deployed to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/hsr-ba-fs15-dat/ba-doc.git" master:gh-pages > /dev/null 2>&1
