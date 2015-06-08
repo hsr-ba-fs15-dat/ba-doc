0272b99734e688441b4f563516c32ffb15ead52a
Remo Liebi <rliebi@hsr.ch>
2015-06-08 17:42:39 +0200

    lots of typos, ignoring english output on title page when built with travis

--- create_change_date.sh
+++ create_change_date.sh
@@ -1,12 +1,7 @@
 if date --version >/dev/null 2>&1 ; then
-    echo "Using GNU date"
-    if $TRAVIS; then
-    	sudo update-locale LANG=de_DE
-	fi
     LANG=DE_de date -d@"$(git log -1 --pretty=format:%ct)" "+%e. %B %Y %H:%M"
 
 else
-    echo "Not using GNU date"
     LANG=DE_de date -r "$(git log -1 --pretty=format:%ct)" "+%e. %B %Y %H:%M"
 fi
 
